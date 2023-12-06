83002228c41e013a05d38d121332470ab1844984
Libberator <madlibberator@gmail.com>
2023-12-06 00:37:48 -0700

    Minor clean-up

--- AdventOfCode/Day06/Day6.cs
+++ AdventOfCode/Day06/Day6.cs
@@ -6,7 +6,7 @@ namespace AoC;
 
 public class Day6(ILogger logger, string path) : Puzzle(logger, path)
 {
-    private long[] _times = Array.Empty<long>();
+    private int[] _times = Array.Empty<int>();
     private long[] _distances = Array.Empty<long>();
 
     public override void Setup()
@@ -14,7 +14,7 @@ public class Day6(ILogger logger, string path) : Puzzle(logger, path)
         var data = ReadAllLines();
         var times = Regex.Matches(data[0], @"(\d+)");
         var dists = Regex.Matches(data[1], @"(\d+)");
-        _times = times.Select(m => long.Parse(m.Value)).ToArray();
+        _times = times.Select(m => int.Parse(m.Value)).ToArray();
         _distances = dists.Select(m => long.Parse(m.Value)).ToArray();
     }
 
@@ -22,19 +22,18 @@ public class Day6(ILogger logger, string path) : Puzzle(logger, path)
 
     public override void SolvePart2()
     {
-        var time = _times.Aggregate((a, b) => a * NearestPowerOf10(b) + b);
+        var time = _times.Aggregate((a, b) => a * (int)NearestPowerOf10(b) + b);
         var distance = _distances.Aggregate((a, b) => a * NearestPowerOf10(b) + b);
         _logger.Log(WaysToWin(time, distance));
     }
 
-    private static long WaysToWin(long time, long distance)
+    private static long WaysToWin(int time, long distance)
     {
-        var waysToWin = 0;
-        for (var i = 1; i < time; i++)
-            if (i * (time - i) > distance) waysToWin++;
-        return waysToWin;
+        for (long i = 1; i < time; i++)
+            if (i * (time - i) > distance) return time - 2 * i + 1;
+        return 0;
     }
 
     // 0-9 -> 10, 10-99 -> 100, 100-999 -> 1000, 1000-9999 -> 10000, etc.
-    private static long NearestPowerOf10(long number) => 10 * (long)Math.Pow(10, (int)Math.Floor(Math.Log10(number)));
+    private static long NearestPowerOf10(long number) => 10 * (long)Math.Pow(10, Math.Floor(Math.Log10(number)));
 }
