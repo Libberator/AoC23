8c2797172a3677b11e7b5b0f9f184ff53da2e4b4
Libberator <madlibberator@gmail.com>
2023-12-07 11:48:40 -0700

    minor clean up

--- AdventOfCode/Day07/Day7.cs
+++ AdventOfCode/Day07/Day7.cs
@@ -7,8 +7,8 @@ namespace AoC;
 public class Day7(ILogger logger, string path) : Puzzle(logger, path)
 {
     private enum HandType { HighCard, OnePair, TwoPair, ThreeOfAKind, FullHouse, FourOfAKind, FiveOfAKind };
-    private readonly char[] _ranking = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
-    private readonly char[] _jokerRanking = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
+    private static readonly char[] _ranking = ['2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K', 'A'];
+    private static readonly char[] _jokerRanking = ['J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A'];
     private readonly List<Hand> _hands = [];
 
     public override void Setup()
@@ -23,32 +23,42 @@ public class Day7(ILogger logger, string path) : Puzzle(logger, path)
 
     public override void SolvePart1()
     {
-        int rank = 1;
-        _hands.Sort((a, b) => CompareHands(a, b, _ranking));
-        var total = _hands.Aggregate(0, (total, h) => total += h.Bid * rank++); ;
-        _logger.Log(total);
+        _hands.Sort(CompareHands);
+        _logger.Log(GetTotalWinnings(_hands));
     }
 
     public override void SolvePart2()
     {
-        int rank = 1;
-        _hands.Sort((a, b) => CompareHands(a, b, _jokerRanking, withJoker: true));
-        var total = _hands.Aggregate(0, (total, h) => total += h.Bid * rank++); ;
-        _logger.Log(total);
+        _hands.Sort(CompareJokerHands);
+        _logger.Log(GetTotalWinnings(_hands));
     }
 
-    private static int CompareHands(Hand a, Hand b, char[] rankings, bool withJoker = false)
+    private static int GetTotalWinnings(List<Hand> hands)
     {
-        // First Compare by Hand Type
-        var aHandType = withJoker ? a.HandTypeWithJoker : a.HandType;
-        var bHandType = withJoker ? b.HandTypeWithJoker : b.HandType;
-        if (aHandType != bHandType) return aHandType.CompareTo(bHandType);
+        int rank = 1;
+        return hands.Aggregate(0, (total, h) => total += h.Bid * rank++);
+    }
 
-        // Then Compare by Card according to provided Ranking
+    private static int CompareHands(Hand a, Hand b)
+    {
+        if (a.HandType != b.HandType) return a.HandType.CompareTo(b.HandType);
+
         for (int i = 0; i < a.Cards.Length; i++)
         {
             if (a.Cards[i] == b.Cards[i]) continue;
-            return Array.IndexOf(rankings, a.Cards[i]).CompareTo(Array.IndexOf(rankings, b.Cards[i]));
+            return Array.IndexOf(_ranking, a.Cards[i]).CompareTo(Array.IndexOf(_ranking, b.Cards[i]));
+        }
+        return 0;
+    }
+
+    private static int CompareJokerHands(Hand a, Hand b)
+    {
+        if (a.JokerHandType != b.JokerHandType) return a.JokerHandType.CompareTo(b.JokerHandType);
+
+        for (int i = 0; i < a.Cards.Length; i++)
+        {
+            if (a.Cards[i] == b.Cards[i]) continue;
+            return Array.IndexOf(_jokerRanking, a.Cards[i]).CompareTo(Array.IndexOf(_jokerRanking, b.Cards[i]));
         }
         return 0;
     }
@@ -56,26 +66,26 @@ public class Day7(ILogger logger, string path) : Puzzle(logger, path)
     private record Hand(string Cards, int Bid)
     {
         public readonly HandType HandType = GetHandType(Cards);
-        public readonly HandType HandTypeWithJoker = GetHandTypeWithJoker(Cards);
+        public readonly HandType JokerHandType = GetJokerHandType(Cards);
 
         private static HandType GetHandType(string cards) => cards.ToHashSet().Count switch
         {
             1 => HandType.FiveOfAKind,
-            2 => cards.Count(c => c == cards[0]) is 2 or 3 ? HandType.FullHouse : HandType.FourOfAKind,
-            3 => cards.Any(i => cards.Count(c => c == i) is 3) ? HandType.ThreeOfAKind : HandType.TwoPair,
+            2 => cards.Count(c => c == cards[0]) is 1 or 4 ? HandType.FourOfAKind : HandType.FullHouse,
+            3 => cards.Any(i => cards.Count(c => c == i) == 3) ? HandType.ThreeOfAKind : HandType.TwoPair,
             4 => HandType.OnePair,
             _ => HandType.HighCard
         };
 
-        private static HandType GetHandTypeWithJoker(string cards) => cards.Count(c => c == 'J') switch
+        private static HandType GetJokerHandType(string cards) => cards.Count(c => c == 'J') switch
         {
             5 or 4 => HandType.FiveOfAKind,
-            3 => cards.Count(c => c == cards.First(i => i != 'J')) is 2 ? HandType.FiveOfAKind : HandType.FourOfAKind,
+            3 => cards.ToHashSet().Count is 2 ? HandType.FiveOfAKind : HandType.FourOfAKind,
             2 => cards.ToHashSet().Count switch
             {
                 2 => HandType.FiveOfAKind,
                 3 => HandType.FourOfAKind,
-                4 or _ => HandType.ThreeOfAKind
+                _ => HandType.ThreeOfAKind
             },
             1 => cards.ToHashSet().Count switch
             {
