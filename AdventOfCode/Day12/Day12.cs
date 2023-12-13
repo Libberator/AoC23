644a631fdf9777ea7b3297f539dea51b61e59ea7
Libberator <madlibberator@gmail.com>
2023-12-12 19:31:21 -0700

    Added comments

--- AdventOfCode/Day12/Day12.cs
+++ AdventOfCode/Day12/Day12.cs
@@ -8,6 +8,7 @@ namespace AoC;
 public class Day12(ILogger logger, string path) : Puzzle(logger, path)
 {
     private readonly List<Report> _reports = [];
+    private readonly List<Report> _unfoldedReports = [];
     private const char DAMAGED = '#', WORKING = '.', UNKNOWN = '?';
 
     public override void Setup()
@@ -16,56 +17,50 @@ public class Day12(ILogger logger, string path) : Puzzle(logger, path)
         {
             var split = line.Split(' ');
             var condition = split[0].AsMemory();
-            var pattern = split[1].Split(',').ToIntArray();
-            _reports.Add(new Report(condition, pattern));
+            var groups = split[1].Split(',').ToIntArray();
+            _reports.Add(new Report(condition, groups));
+
+            var unfoldedCondition = string.Join(UNKNOWN, Enumerable.Repeat(condition, 5)).AsMemory();
+            var unfoldedGroups = Enumerable.Repeat(groups, 5).SelectMany(arr => arr).ToArray();
+            _unfoldedReports.Add(new Report(unfoldedCondition, unfoldedGroups));
         }
     }
 
-    public override void SolvePart1() => _logger.Log(_reports.Sum(r => TotalConfigurations(r)));
+    public override void SolvePart1() => _logger.Log(_reports.Sum(r => Recurse(r, [])));
 
-    public override void SolvePart2() => _logger.Log(_reports.Sum(r => TotalConfigurations(r, 5)));
-
-    private long TotalConfigurations(Report report, int repeats = 1)
-    {
-        if (repeats == 1) return Recurse(report, []);
-
-        var repeatedCondition = string.Join(UNKNOWN, Enumerable.Repeat(report.Condition, repeats));
-        var repeatedPattern = Enumerable.Repeat(report.Pattern, repeats).SelectMany(arr => arr).ToArray();
-        var expandedReport = new Report(repeatedCondition.AsMemory(), repeatedPattern);
-        return Recurse(expandedReport, []);
-    }
+    public override void SolvePart2() => _logger.Log(_unfoldedReports.Sum(r => Recurse(r, [])));
 
     private static long Recurse(Report report, Dictionary<Report, long> cache)
     {
         if (cache.TryGetValue(report, out var cachedTotal)) return cachedTotal;
 
         var condition = report.Condition;
-        var pattern = report.Pattern;
-        if (pattern.Length == 0) return condition.Span.Contains(DAMAGED) ? 0 : 1; // fail vs success
+        var groups = report.Groups;
+        if (groups.Length == 0) return condition.Span.Contains(DAMAGED) ? 0 : 1; // fail vs success
 
-        int chunk = pattern[0];
-        var latestIndex = condition.Length - (pattern.Sum() + pattern.Length) + 1;
+        int group = groups[0];
+        var latestIndex = condition.Length - (groups.Sum() + groups.Length) + 1; // furthest we can slide window
 
         long total = 0;
         for (int i = 0; i <= latestIndex; i++)
         {
             // check for all the early outs to prune branches
             if (condition.Span[..i].Contains(DAMAGED)) break; // can't skip over a known damaged spring, '#'
-            var endIndex = i + chunk;
+            var endIndex = i + group;
             if (condition[i..endIndex].Span.Contains(WORKING)) continue; // can't slide this window on a '.'
-            if (endIndex >= condition.Length) return total + 1; // reached end. yay!
+            if (endIndex >= condition.Length) return total + 1; // this successful group reached end. yay!
             if (condition.Span[endIndex] == DAMAGED) continue; // next char is not a '.' or '?'. that's a no-no
 
-            total += Recurse(report.Next(endIndex + 1), cache); // +1 because we need a '.' spacing between chunks
+            total += Recurse(report.Next(endIndex + 1), cache); // +1 because we need a '.' spacing between groups
         }
         cache.Add(report, total);
         return total;
     }
 
-    private record struct Report(ReadOnlyMemory<char> Condition, int[] Pattern) : IEquatable<Report>
+    private readonly record struct Report(ReadOnlyMemory<char> Condition, int[] Groups) : IEquatable<Report>
     {
-        public readonly Report Next(int startIndex) => new(Condition[startIndex..].TrimStart(WORKING), Pattern[1..]);
-        public readonly bool Equals(Report other) => Condition.Span.SequenceEqual(other.Condition.Span) && StructuralComparisons.StructuralEqualityComparer.Equals(Pattern, other.Pattern);
-        public readonly override int GetHashCode() => HashCode.Combine(Condition, StructuralComparisons.StructuralEqualityComparer.GetHashCode(Pattern));
+        public readonly Report Next(int startIndex) => new(Condition[startIndex..].TrimStart(WORKING), Groups[1..]);
+        public readonly bool Equals(Report other) => Condition.Span.SequenceEqual(other.Condition.Span) && StructuralComparisons.StructuralEqualityComparer.Equals(Groups, other.Groups);
+        public readonly override int GetHashCode() => HashCode.Combine(Condition, StructuralComparisons.StructuralEqualityComparer.GetHashCode(Groups));
     }
 }
