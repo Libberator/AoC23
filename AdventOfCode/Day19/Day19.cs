e321055c0d53d9518137200051f1aa8317de7767
Libberator <madlibberator@gmail.com>
2023-12-19 01:52:18 -0700

    Cleaned up Day 19

--- AdventOfCode/Day19/Day19.cs
+++ AdventOfCode/Day19/Day19.cs
@@ -1,47 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace AoC;
 
 public class Day19(ILogger logger, string path) : Puzzle(logger, path)
 {
-    private record struct Part(int X, int M, int A, int S)
-    {
-        public readonly int Total = X + M + A + S;
-    }
+    private const string ACCEPTED = "A", REJECTED = "R", START = "in";
 
-    private class Workflow()
-    {
-        public readonly List<Rule> Rules = [];
-        public string IfNonePass = "";
-    }
-
-    private record struct Rule(Condition Condition, string ResultIfTrue);
-    private record struct Condition(string Letter, bool IsGreaterThan, int Value)
-    {
-        public readonly bool PassesCondition(Part part)
-        {
-            var partValue = Letter switch
-            {
-                "x" => part.X,
-                "m" => part.M,
-                "a" => part.A,
-                "s" => part.S,
-                _ => throw new IndexOutOfRangeException()
-            };
-            return IsGreaterThan ? partValue > Value : partValue < Value;
-        }
-    }
-
-    private readonly List<Part> _parts = [];
     private readonly Dictionary<string, Workflow> _workflows = [];
-    private Workflow? _startingWorkflow;
+    private readonly List<Part> _parts = [];
 
     public override void Setup()
     {
         bool firstPart = true;
-
         foreach (var line in ReadFromFile())
         {
             if (string.IsNullOrEmpty(line))
@@ -50,181 +23,174 @@ public class Day19(ILogger logger, string path) : Puzzle(logger, path)
                 continue;
             }
 
-            if (firstPart) // workflows
-            {
-                var split = line.Split('{');
-                var id = split[0];
-                var rules = split[1][..^1].Split(',');
-
-                var workflow = new Workflow();
-
-                for (int i = 0; i < rules.Length - 1; i++)
-                {
-                    var match = Regex.Match(rules[i], @"(\w)([><])(\d+):(\w+)");
-                    var letter = match.Groups[1].Value;
-                    var isGreaterSymbol = match.Groups[2].Value;
-                    var number = int.Parse(match.Groups[3].Value);
-                    var ifTrue = match.Groups[4].Value;
-
-                    var condition = new Condition(letter, isGreaterSymbol == ">", number);
-                    var rule = new Rule(condition, ifTrue);
-                    workflow.Rules.Add(rule);
-                }
-                var ifAllFalse = rules[^1];
-                workflow.IfNonePass = ifAllFalse;
-                _workflows.Add(id, workflow);
-                if (id == "in") _startingWorkflow = workflow;
-            }
-            else // parts
-            {
-                var matches = Regex.Matches(line, @"\d+");
-                var x = int.Parse(matches[0].Value);
-                var m = int.Parse(matches[1].Value);
-                var a = int.Parse(matches[2].Value);
-                var s = int.Parse(matches[3].Value);
-
-                var workflow = new Part(x, m, a, s);
-                _parts.Add(workflow);
-            }
+            if (firstPart)
+                ParseWorkflows(line);
+            else
+                ParseParts(line);
         }
     }
 
-    public override void SolvePart1()
+    private void ParseWorkflows(string line)
     {
-        int total = 0;
-        foreach (var part in _parts)
-        {
-            if (IsAcceptedPart(part, _startingWorkflow!))
-                total += part.Total;
-        }
+        var split = line.Split('{');
+        var id = split[0];
+        var rules = split[1][..^1].Split(',');
 
-        _logger.Log(total);
+        var workflow = new Workflow(failResult: rules[^1]);
+        _workflows.Add(id, workflow);
+
+        for (int i = 0; i < rules.Length - 1; i++)
+        {
+            var rule = rules[i];
+            char letter = rule[0];
+            bool isGreaterThan = rule[1] == '>';
+
+            var match = Regex.Match(rule, @"(\d+):(\w+)");
+            int value = int.Parse(match.Groups[1].Value);
+            string passResult = match.Groups[2].Value;
+
+            var condition = new Condition(letter, isGreaterThan, value, passResult);
+            workflow.Conditions.Add(condition);
+        }
+    }
+
+    private void ParseParts(string line)
+    {
+        var matches = Regex.Matches(line, @"\d+");
+        var x = int.Parse(matches[0].Value);
+        var m = int.Parse(matches[1].Value);
+        var a = int.Parse(matches[2].Value);
+        var s = int.Parse(matches[3].Value);
+        _parts.Add(new Part(x, m, a, s));
+    }
+
+    public override void SolvePart1() =>
+        _logger.Log(_parts.Sum(p => IsAcceptedPart(p, _workflows[START]) ? p.Total : 0));
+
+    public override void SolvePart2()
+    {
+        var range = new Range(1, 4000);
+        var partRange = new PartRange(range, range, range, range);
+        List<PartRange> validRanges = [];
+
+        ProcessRange(partRange, _workflows[START], validRanges);
+
+        _logger.Log(validRanges.Sum(r => r.Total));
     }
 
     private bool IsAcceptedPart(Part part, Workflow workflow)
     {
-        foreach (var rule in workflow.Rules)
+        foreach (var condition in workflow.Conditions)
         {
-            if (rule.Condition.PassesCondition(part))
+            if (condition.Passes(part))
             {
-                var result = rule.ResultIfTrue;
-                if (result == "A") return true;
-                if (result == "R") return false;
-
-                return IsAcceptedPart(part, _workflows[result]);
+                return condition.PassResult switch
+                {
+                    ACCEPTED => true,
+                    REJECTED => false,
+                    _ => IsAcceptedPart(part, _workflows[condition.PassResult])
+                };
             }
         }
-        // none of the rules passed
-        var ifNonePass = workflow.IfNonePass;
-        if (ifNonePass == "A") return true;
-        if (ifNonePass == "R") return false;
-        return IsAcceptedPart(part, _workflows[ifNonePass]);
+
+        return workflow.FailResult switch
+        {
+            ACCEPTED => true,
+            REJECTED => false,
+            _ => IsAcceptedPart(part, _workflows[workflow.FailResult])
+        };
     }
 
-    public override void SolvePart2()
+    private void ProcessRange(PartRange fail, Workflow workflow, List<PartRange> validRanges)
     {
-        long total = 0;
-
-        var range = new LongRange(1, 4000);
-        var partRange = new PartRange(range, range, range, range);
-        var workflow = _workflows["in"];
-
-        List<PartRange> validRanges = [];
-
-        ProcessRange(partRange, workflow, validRanges);
-
-        foreach (var validRange in validRanges)
+        foreach (var condition in workflow.Conditions)
         {
-            total += validRange.Total;
-        }
-
-        _logger.Log(total);
-    }
-
-    private void ProcessRange(PartRange range, Workflow workflow, List<PartRange> validRanges)
-    {
-        if (range.Total == 0) return;
-        // "failing" remainder values
-        var fail = range;
-
-        foreach (var rule in workflow.Rules)
-        {
-            var condition = rule.Condition;
-            var resultIfTrue = rule.ResultIfTrue;
-
             var pass = fail; // copy current failing version
+            var letter = condition.Letter; // use as an indexer
 
-            switch (condition.Letter)
+            if (condition.IsGreaterThan)
             {
-                case "x":
-                    if (condition.IsGreaterThan)
-                        fail.X.Split(condition.Value, out fail.X, out pass.X);
-                    else
-                        fail.X.Split(condition.Value - 1, out pass.X, out fail.X);
-                    break;
-                case "m":
-                    if (condition.IsGreaterThan)
-                        fail.M.Split(condition.Value, out fail.M, out pass.M);
-                    else
-                        fail.M.Split(condition.Value - 1, out pass.M, out fail.M);
-                    break;
-                case "a":
-                    if (condition.IsGreaterThan)
-                        fail.A.Split(condition.Value, out fail.A, out pass.A);
-                    else
-                        fail.A.Split(condition.Value - 1, out pass.A, out fail.A);
-                    break;
-                case "s":
-                    if (condition.IsGreaterThan)
-                        fail.S.Split(condition.Value, out fail.S, out pass.S);
-                    else
-                        fail.S.Split(condition.Value - 1, out pass.S, out fail.S);
-                    break;
+                fail[letter].Split(condition.Value, out var failRange, out var passRange);
+                fail[letter] = failRange;
+                pass[letter] = passRange;
+            }
+            else
+            {
+                fail[letter].Split(condition.Value - 1, out var passRange, out var failRange);
+                fail[letter] = failRange;
+                pass[letter] = passRange;
             }
 
-            if (resultIfTrue == "A")
-            {
-                if (pass.Total != 0)
-                    validRanges.Add(pass); // yay
-                continue;
-            }
-            if (resultIfTrue == "R") continue; // end branch
-            ProcessRange(pass, _workflows[resultIfTrue], validRanges); // split into valid path
+            if (condition.PassResult == ACCEPTED)
+                validRanges.Add(pass);
+            else if (condition.PassResult != REJECTED)
+                ProcessRange(pass, _workflows[condition.PassResult], validRanges);
         }
 
-        // none of the rules passed
-        var ifNonePass = workflow.IfNonePass;
-        if (ifNonePass == "A")
-        {
-            if (fail.Total != 0)
-                validRanges.Add(fail);
-            return;
-        }
-        if (ifNonePass == "R") return;
-        ProcessRange(fail, _workflows[ifNonePass], validRanges);
+        if (workflow.FailResult == ACCEPTED)
+            validRanges.Add(fail);
+        else if (workflow.FailResult != REJECTED)
+            ProcessRange(fail, _workflows[workflow.FailResult], validRanges);
     }
 
-
-    private struct PartRange(LongRange x, LongRange m, LongRange a, LongRange s)
+    private class Workflow(string failResult)
     {
-        public LongRange X = x;
-        public LongRange M = m;
-        public LongRange A = a;
-        public LongRange S = s;
-
-        public readonly long Total => X.Length * M.Length * A.Length * S.Length;
+        public readonly List<Condition> Conditions = [];
+        public readonly string FailResult = failResult;
     }
 
-    private record struct LongRange(long Start, long End)
+    private record struct Condition(char Letter, bool IsGreaterThan, int Value, string PassResult)
+    {
+        public readonly bool Passes(Part part) =>
+            IsGreaterThan ? part[Letter] > Value : part[Letter] < Value;
+    }
+
+    private record struct Part(int X, int M, int A, int S)
+    {
+        public readonly int Total = X + M + A + S;
+        public readonly int this[char letter] => letter switch
+        {
+            'x' => X,
+            'm' => M,
+            'a' => A,
+            's' => S,
+            _ => throw new IndexOutOfRangeException()
+        };
+    }
+
+    private record struct Range(int Start, int End) // End is inclusive
     {
         public readonly long Length => End - Start + 1;
+        public readonly void Split(int value, out Range left, out Range right) =>
+            (left, right) = (new Range(Start, value), new Range(value + 1, End));
+    }
 
-        public readonly void Split(long value, out LongRange left, out LongRange right)
+    private struct PartRange(Range x, Range m, Range a, Range s)
+    {
+        public Range X = x, M = m, A = a, S = s;
+        public readonly long Total => X.Length * M.Length * A.Length * S.Length;
+
+        public Range this[char letter]
         {
-            var rightResult = new LongRange(value + 1, End);
-            var leftResult = new LongRange(Start, value);
-            right = rightResult;
-            left = leftResult;
+            readonly get => letter switch
+            {
+                'x' => X,
+                'm' => M,
+                'a' => A,
+                's' => S,
+                _ => throw new IndexOutOfRangeException()
+            };
+            set
+            {
+                switch (letter)
+                {
+                    case 'x': X = value; break;
+                    case 'm': M = value; break;
+                    case 'a': A = value; break;
+                    case 's': S = value; break;
+                    default: throw new IndexOutOfRangeException();
+                }
+            }
         }
     }
 }
