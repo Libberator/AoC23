5d32da7d09e7f5f0840b41dfddc7926d2cbab7c9
Libberator <madlibberator@gmail.com>
2023-12-26 15:48:29 -0700

    Refactored Day21 to use quadratic extrapolation

--- AdventOfCode/Day21/Day21.cs
+++ AdventOfCode/Day21/Day21.cs
@@ -12,7 +12,7 @@ public class Day21(ILogger logger, string path) : Puzzle(logger, path)
     public override void Setup()
     {
         _grid = ReadAllLines();
-        
+
         for (int row = 0; row < _grid.Length; row++)
         {
             var line = _grid[row];
@@ -25,54 +25,35 @@ public class Day21(ILogger logger, string path) : Puzzle(logger, path)
         }
     }
 
-    public override void SolvePart1() => _logger.Log(TakeSteps(_grid, _startPos, 64).Even);
+    public override void SolvePart1() => _logger.Log(TakeSteps(_grid, _startPos, 64)); // for test, use 6. Expected test result: 16
 
     // TODO: Change to quadratic extrapolation
     public override void SolvePart2()
     {
-        long steps = 26501365;
+        int steps = 26501365;
         int size = _grid.Length; // 131, odd-sized square -> more even potential plots than odds
+        var grids = steps / size; // 202300
 
-        // -------- FULL GRIDS ------------
-        // figure out how many total grids get filled up - split into even and odd grids
-        long metaGridWidth = steps / size; // 202,300
-        var fullEvenMetaGrids = metaGridWidth * metaGridWidth; // 40925290000
-        var fullOddMetaGrids = (metaGridWidth - 1) * (metaGridWidth - 1); // 40924885401
+        // get 3 mapped (x,y) points
+        var x1 = steps % size; // 65
+        var y1 = TakeSteps(_grid, _startPos, x1);
+        var x2 = x1 + size; // 196
+        var y2 = TakeSteps(_grid, _startPos, x2);
+        var x3 = x2 + size; // 327
+        var y3 = TakeSteps(_grid, _startPos, x3);
 
-        // fill up the grid to get the even and odd distributions
-        var stepsToFillFromCenter = size - 1; // ignoring starting point, step 65 laterally, then 65 more to the corner
-        // _startPos is at the center of the grid
-        var (even, odd, _) = TakeSteps(_grid, _startPos, stepsToFillFromCenter); // 7566, 7509 (total: 15075)
+        // with 3 mapped (x,y) points, we can use the quadratic formula to extrapolate another y value given x
+        // solve for a, b, and c in the equation y = a*x^2 + b*x + c
+        // assumption: we simplify the formulas by saying that x1 is 0, x2 is 1, and x3 is 2
+        var a = (y3 - 2 * y2 + y1) / 2;
+        var b = y2 - y1 - a;
+        var c = y1;
 
-        // --------- PARTIAL GRIDS (edge cases, literally) ------------
-        // partially fill up the 4 tips of the furthest traveled diamond corners
-        var partialTop = TakeSteps(_grid, new Vector2Int(_startPos.X, size - 1), size - 1).Value;
-        var partialRight = TakeSteps(_grid, new Vector2Int(0, _startPos.Y), size - 1).Value;
-        var partialBottom = TakeSteps(_grid, new Vector2Int(_startPos.X, 0), size - 1).Value;
-        var partialLeft = TakeSteps(_grid, new Vector2Int(size - 1, _startPos.Y), size - 1).Value;
-
-        // small cutout triangles of a grid along the edge
-        var smallTR = TakeSteps(_grid, new Vector2Int(size - 1, 0), size / 2 - 1).Value;
-        var smallTL = TakeSteps(_grid, new Vector2Int(size - 1, size - 1), size / 2 - 1).Value;
-        var smallBR = TakeSteps(_grid, new Vector2Int(0, 0), size / 2 - 1).Value;
-        var smallBL = TakeSteps(_grid, new Vector2Int(0, size - 1), size / 2 - 1).Value;
-
-        // larger "home-plate"-shaped cutout
-        var largeTR = TakeSteps(_grid, new Vector2Int(size - 1, 0), 3 * size / 2 - 1).Value;
-        var largeTL = TakeSteps(_grid, new Vector2Int(size - 1, size - 1), 3 * size / 2 - 1).Value;
-        var largeBR = TakeSteps(_grid, new Vector2Int(0, 0), 3 * size / 2 - 1).Value;
-        var largeBL = TakeSteps(_grid, new Vector2Int(0, size - 1), 3 * size / 2 - 1).Value;
-
-        // ---------- TOTAL -----------
-        var total = fullOddMetaGrids * odd + fullEvenMetaGrids * even
-            + partialTop + partialRight + partialBottom + partialLeft
-            + metaGridWidth * (smallTR + smallTL + smallBR + smallBL)
-            + (metaGridWidth - 1) * (largeTR + largeTL + largeBR + largeBL);
-
+        var total = a * grids * grids + b * grids + c;
         _logger.Log(total); // 616951804315987
     }
 
-    private static (long Even, long Odd, long Value) TakeSteps(string[] grid, Vector2Int startPos, int steps)
+    private static long TakeSteps(string[] grid, Vector2Int startPos, int steps)
     {
         long totalEvenPlots = 1;
         long totalOddPlots = 0;
@@ -86,13 +67,12 @@ public class Day21(ILogger logger, string path) : Puzzle(logger, path)
             else totalOddPlots += newPlots;
         }
 
-        return (totalEvenPlots, totalOddPlots, steps % 2 == 0 ? totalEvenPlots : totalOddPlots);
+        return steps % 2 == 0 ? totalEvenPlots : totalOddPlots;
     }
 
     private static int Step(string[] grid, HashSet<Vector2Int> odd, HashSet<Vector2Int> even, int stepNumber)
     {
-        var (nextSteps, startFrom) = stepNumber % 2 == 0 ?
-            (even, odd) : (odd, even);
+        var (nextSteps, startFrom) = stepNumber % 2 == 0 ? (even, odd) : (odd, even);
         var toRemove = new HashSet<Vector2Int>(nextSteps);
 
         List<Vector2Int> newEndPoints = [];
@@ -102,7 +82,7 @@ public class Day21(ILogger logger, string path) : Puzzle(logger, path)
             {
                 var nextPos = pos + dir;
                 if (nextSteps.Contains(nextPos)) continue;
-                if (!CanVisit(nextPos, grid)) continue;
+                if (!CanVisit(grid, nextPos)) continue;
 
                 nextSteps.Add(nextPos);
             }
@@ -111,8 +91,10 @@ public class Day21(ILogger logger, string path) : Puzzle(logger, path)
         return nextSteps.Count;
     }
 
-    private static bool CanVisit(Vector2Int nextPos, string[] grid) => 
-        nextPos.X >= 0 && nextPos.Y >= 0
-        && nextPos.X < grid.Length && nextPos.Y < grid[0].Length
-        && grid[nextPos.X][nextPos.Y] != ROCK;
+    private static bool CanVisit(string[] grid, Vector2Int nextPos)
+    {
+        var xPos = nextPos.X.Mod(grid.Length);
+        var yPos = nextPos.Y.Mod(grid[0].Length);
+        return grid[xPos][yPos] != ROCK;
+    }
 }
