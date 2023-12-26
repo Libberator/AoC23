390c065e2b5df6fa0e4e0101593aaaf9204820ea
Libberator <madlibberator@gmail.com>
2023-12-26 16:16:05 -0700

    Cleaned up Day21

--- AdventOfCode/Day21/Day21.cs
+++ AdventOfCode/Day21/Day21.cs
@@ -1,73 +1,65 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 
 namespace AoC;
 
 public class Day21(ILogger logger, string path) : Puzzle(logger, path)
 {
-    private const char START = 'S', ROCK = '#';
+    private const char ROCK = '#', START = 'S';
     private string[] _grid = [];
     private Vector2Int _startPos;
 
     public override void Setup()
     {
         _grid = ReadAllLines();
-
-        for (int row = 0; row < _grid.Length; row++)
-        {
-            var line = _grid[row];
-            var startIndex = line.IndexOf(START);
-            if (startIndex != -1)
-            {
-                _startPos = new Vector2Int(row, startIndex);
-                break;
-            }
-        }
+        _startPos = new Vector2Int(_grid.Length / 2, _grid[0].Length / 2); // in the center
     }
 
-    public override void SolvePart1() => _logger.Log(TakeSteps(_grid, _startPos, 64)); // for test, use 6. Expected test result: 16
+    // for test, use 6. Expected test result: 16
+    public override void SolvePart1() => _logger.Log(TakeSteps(_grid, _startPos, 64));
 
-    // TODO: Change to quadratic extrapolation
     public override void SolvePart2()
     {
         int steps = 26501365;
-        int size = _grid.Length; // 131, odd-sized square -> more even potential plots than odds
-        var grids = steps / size; // 202300
+        int size = _grid.Length; // 131
+        var rem = steps % size; // 65
 
-        // get 3 mapped (x,y) points
-        var x1 = steps % size; // 65
-        var y1 = TakeSteps(_grid, _startPos, x1);
-        var x2 = x1 + size; // 196
-        var y2 = TakeSteps(_grid, _startPos, x2);
-        var x3 = x2 + size; // 327
-        var y3 = TakeSteps(_grid, _startPos, x3);
+        // get 3 mapped (x, y) points at x1 = 65, x2 = 196, x3 = 327
+        var seq = TakeSteps(_grid, _startPos, [rem, rem + size, rem + 2 * size]).ToArray();
 
-        // with 3 mapped (x,y) points, we can use the quadratic formula to extrapolate another y value given x
-        // solve for a, b, and c in the equation y = a*x^2 + b*x + c
-        // assumption: we simplify the formulas by saying that x1 is 0, x2 is 1, and x3 is 2
-        var a = (y3 - 2 * y2 + y1) / 2;
-        var b = y2 - y1 - a;
-        var c = y1;
+        // we can use the quadratic formula to extrapolate another y value given x
+        // solve for a, b, and c with 3 linear equations of y = a*x^2 + b*x + c
+        // To simplify: assume that x1 is equal to 0, x2 is 1, and x3 is 2
+        var a = (seq[2] - 2 * seq[1] + seq[0]) / 2;
+        var b = seq[1] - seq[0] - a;
+        var c = seq[0];
 
-        var total = a * grids * grids + b * grids + c;
+        var x = steps / size; // 202300
+        var total = (a * x * x) + (b * x) + c;
         _logger.Log(total); // 616951804315987
     }
 
-    private static long TakeSteps(string[] grid, Vector2Int startPos, int steps)
+    private static long TakeSteps(string[] grid, Vector2Int startPos, int steps) =>
+        TakeSteps(grid, startPos, [steps]).First();
+
+    private static IEnumerable<long> TakeSteps(string[] grid, Vector2Int startPos, int[] steps)
     {
         long totalEvenPlots = 1;
         long totalOddPlots = 0;
         HashSet<Vector2Int> oddSteps = [];
         HashSet<Vector2Int> evenSteps = [startPos];
 
-        for (int i = 1; i <= steps; i++)
+        var maxSteps = steps.MaxBy(x => x);
+        for (int i = 1; i <= maxSteps; i++)
         {
             var newPlots = Step(grid, oddSteps, evenSteps, i);
             if (i % 2 == 0) totalEvenPlots += newPlots;
             else totalOddPlots += newPlots;
-        }
 
-        return steps % 2 == 0 ? totalEvenPlots : totalOddPlots;
+            if (steps.Contains(i))
+                yield return i % 2 == 0 ? totalEvenPlots : totalOddPlots;
+        }
     }
 
     private static int Step(string[] grid, HashSet<Vector2Int> odd, HashSet<Vector2Int> even, int stepNumber)
@@ -93,8 +85,8 @@ public class Day21(ILogger logger, string path) : Puzzle(logger, path)
 
     private static bool CanVisit(string[] grid, Vector2Int nextPos)
     {
-        var xPos = nextPos.X.Mod(grid.Length);
-        var yPos = nextPos.Y.Mod(grid[0].Length);
+        var xPos = nextPos.X.Mod(grid.Length); // this Mod method will loop around like Pacman
+        var yPos = nextPos.Y.Mod(grid[0].Length); // and handles negatives better than %
         return grid[xPos][yPos] != ROCK;
     }
 }
